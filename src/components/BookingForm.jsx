513071dcc4108f2fdc9469762e60d3bfaf2ce82d
Volodymyr Kashnikov <vkashnikov19@gmail.com>
2024-12-27 11:49:38 +0200

    update files

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import styles from './BookingForm.module.css';
-import toast from 'react-hot-toast';
+import styles from "./BookingForm.module.css";
+import toast from "react-hot-toast";
 const BookingForm = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -8,7 +8,6 @@ const BookingForm = () => {
     date: "",
     comment: "",
   });
-
 
   // Handle input changes
   const handleChange = (event) => {
@@ -22,58 +21,60 @@ const BookingForm = () => {
   // Handle form submission
   const handleSubmit = (event) => {
     event.preventDefault();
-    toast.success('Successfully!');
-    };
+    toast.success("Successfully!");
+  };
 
   return (
     <div className={styles.bookingForm}>
       <h2>Book your campervan now</h2>
       <p>Stay connected! We are always ready to help you.</p>
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <input
-            id="name"
-            type="text"
-            name="name"
-            placeholder="Name*"
-            required
-            value={formData.name}
-            onChange={handleChange}
-            className={styles.input}
-          />
-          <input
-            id="email"
-            type="email"
-            name="email"
-            placeholder="Email*"
-            required
-            value={formData.email}
-            onChange={handleChange}
-            className={styles.input}
-          />
-          <input
-            id="date"
-            type="text"
-            name="date"
-            placeholder="Booking date*"
-            required
-            value={formData.date}
-            onFocus={(e) => (e.target.type = "date")}
-            onBlur={(e) => (e.target.type = "text")}
-            onChange={handleChange}
-            className={styles.input}
-          />
-          <textarea
-            id="comment"
-            name="comment"
-            placeholder="Comment"
-            value={formData.comment}
-            onChange={handleChange}
-            className={styles.textarea}
-          />
-          <div className={styles.buttonContainer}>
-            <button type="submit" className={styles.submitButton}>Send</button>
-          </div>
-        </form>       
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          placeholder="Name*"
+          required
+          value={formData.name}
+          onChange={handleChange}
+          className={styles.input}
+        />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Email*"
+          required
+          value={formData.email}
+          onChange={handleChange}
+          className={styles.input}
+        />
+        <input
+          id="date"
+          type="text"
+          name="date"
+          placeholder="Booking date*"
+          required
+          value={formData.date}
+          onFocus={(e) => (e.target.type = "date")}
+          onBlur={(e) => (e.target.type = "text")}
+          onChange={handleChange}
+          className={styles.input}
+        />
+        <textarea
+          id="comment"
+          name="comment"
+          placeholder="Comment"
+          value={formData.comment}
+          onChange={handleChange}
+          className={styles.textarea}
+        />
+        <div className={styles.buttonContainer}>
+          <button type="submit" className={styles.submitButton}>
+            Send
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
